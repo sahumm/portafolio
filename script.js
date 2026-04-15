1934a2790efb20e59edd730dc893421a2c4f4466
sahumm <sahu0408asiel@gmail.com>
2026-04-14 18:58:58 -0700

    mejorado

--- script.js
+++
@@ -1,6 +0,0 @@
-// No había JavaScript en el archivo original.
-// Puedes agregar aquí interactividad futura, por ejemplo:
-
-// document.querySelectorAll('.proj-item').forEach(item => {
-//   item.addEventListener('click', () => { ... });
-// });
